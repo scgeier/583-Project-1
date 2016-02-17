7e7a804d98192e87430268b7f3c6544c3e1f52d9
scgeier <scgeier@gmail.com>
2016-02-17 14:40:12 -0500

    8th commit:Added 'about' popup and started css

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 
 var app = angular.module('myApp', ['seats']);
 
+angular.element(document).ready(function () {
+    $('#info-icon').click(function(){
+         $('#about').fadeToggle( "fast", "linear" );
+            });
+    
+     $('.box-closer').click(function(){
+        $('#about').hide();
+        });
+});
+
 app.controller('BaseController', function() {
 
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
     console.log("DOM loaded!");
     
-   $('.clickable').click(function(){
-          $('#hidden-seat').fadeToggle( "fast", "linear" );
-        });
+
 });
